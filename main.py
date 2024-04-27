adfadf38204474a70d31f51ef7206edf2d7ec948
federicocamino <142539862+federicocamino@users.noreply.github.com>
2024-04-27 17:08:48 -0300

    commit 4

--- main.py
+++ main.py
@@ -14,17 +14,19 @@ genres=df_genres['Genre'].to_list()
 @app.get("/PlayTimeGenre/{genero}")
 def PlayTimeGenre( genero : str ):
     # mensaje si se ingresa un tipo incorrecto o bien no se escribe bien el género. Devuelve el listado de géneros.
-    if (type(genero)!= str) or (genero not in genres): print("No se encuentra el género indicado. \nEste es un listado de los géneros con los que contamos: \n" + ', '.join(genres))
+    if (type(genero)!= str) or (genero not in genres): 
+        return str("No se encuentra el género indicado. \nEste es un listado de los géneros con los que contamos: \n" + ', '.join(genres))
     else:
         Anio= df_genres[df_genres['Genre']==genero]['RYMaxHours'].item()
-        print('{"Año de lanzamiento con más horas jugadas para Género X" : '+str(Anio)+'}')
+        return('{"Año de lanzamiento con más horas jugadas para Género X" : '+str(Anio)+'}')
 
 
 @app.get("/usergenre/{genero}")
 def UserForGenre( genero : str ):
     # mensaje si se ingresa un tipo incorrecto o bien no se escribe bien el género. Devuelve el listado de géneros.
-    if (type(genero)!= str) or (genero not in genres): print("No se encuentra el género indicado. \nEste es un listado de los géneros con los que contamos: \n" + ', '.join(genres))
+    if (type(genero)!= str) or (genero not in genres): 
+        return ("No se encuentra el género indicado. \nEste es un listado de los géneros con los que contamos: \n" + ', '.join(genres))
     else:
         jugador= df_genres[df_genres['Genre']==genero]['PlayerMH'].item()
         detalleanio= df_genres[df_genres['Genre']==genero]['Years Played by player'].item()
-        print('{"Usuario con más horas jugadas para Género X" : '+str(jugador)+', \n"Horas jugadas":'+str(detalleanio))
+        return('{"Usuario con más horas jugadas para Género X" : '+str(jugador)+', \n"Horas jugadas":'+str(detalleanio))
