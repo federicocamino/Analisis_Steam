c4c2aba7a0c68a05a5def174fcdafdfea0bc7100
federicocamino <142539862+federicocamino@users.noreply.github.com>
2024-05-08 01:50:06 -0300

    formato main

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ genres=df_genres['Genre'].to_list()
 df_recommended_gamesxgames=pd.read_csv('Datos_procesados/df_recommended_gamesxgames.csv')
 product_ids=df_recommended_gamesxgames['item_id'].to_list()
 df_year_recommendations=pd.read_csv('Datos_procesados/year_recommendations.csv')
-anios=df_year_recommendations['year'].to_list()
+anios=sorted(df_year_recommendations['year'].to_list())
 df_releaseyear_sentiment=pd.read_csv('Datos_procesados/releaseyear_sentiment.csv')
-anioslanzamiento=df_releaseyear_sentiment['release_year'].to_list()
+anioslanzamiento=sorted(df_releaseyear_sentiment['release_year'].to_list())
 
 
 #Endpoints de la API
@@ -22,7 +22,7 @@ anioslanzamiento=df_releaseyear_sentiment['release_year'].to_list()
 def PlayTimeGenre( genero : str ):
     # mensaje si se ingresa un tipo incorrecto o bien no se escribe bien el género. Devuelve el listado de géneros.
     if (type(genero)!= str) or (genero not in genres): 
-        return str("No se encuentra el género indicado. \nEste es un listado de los géneros con los que contamos: \n" + ', '.join(genres))
+        return str("No se encuentra el género indicado. Este es un listado de los géneros con los que contamos: " + ', '.join(genres))
     else:
         Anio= df_genres[df_genres['Genre']==genero]['RYMaxHours'].item()
         return('{"Año de lanzamiento con más horas jugadas para Género X" : '+str(Anio)+'}')
@@ -32,18 +32,18 @@ def PlayTimeGenre( genero : str ):
 def UserForGenre( genero : str ):
     # mensaje si se ingresa un tipo incorrecto o bien no se escribe bien el género. Devuelve el listado de géneros.
     if (type(genero)!= str) or (genero not in genres): 
-        return ("No se encuentra el género indicado. \nEste es un listado de los géneros con los que contamos: \n" + ', '.join(genres))
+        return ("No se encuentra el género indicado. Este es un listado de los géneros con los que contamos: " + ', '.join(genres))
     else:
         jugador= df_genres[df_genres['Genre']==genero]['PlayerMH'].item()
         detalleanio= df_genres[df_genres['Genre']==genero]['Years Played by player'].item()
-        return('{"Usuario con más horas jugadas para Género X" : '+str(jugador)+', \n "Horas jugadas":'+str(detalleanio))
+        return('{"Usuario con más horas jugadas para Género X" : '+str(jugador)+', "Horas jugadas":'+str(detalleanio))
 
 
 @app.get("/UsersRecommend/{año}")
 def UsersRecommend( anio : int ):
     # mensaje si se ingresa un tipo incorrecto o bien no se escribe bien el género. Devuelve el listado de géneros.
     if (type(anio)!= int) or (anio not in anios): 
-        return ("No se encuentra el año indicado. \nEste es un listado de los años para los cuales tenemos información: \n" + str(anios))
+        return ("No se encuentra el año indicado. Este es un listado de los años para los cuales tenemos información: " + str(anios))
     else:
         reco1 = df_year_recommendations[df_year_recommendations['year']==anio]['recommend_title_1'].item()
         reco2 = df_year_recommendations[df_year_recommendations['year']==anio]['recommend_title_2'].item()
@@ -55,7 +55,7 @@ def UsersRecommend( anio : int ):
 def UsersNotRecommend( anio : int ):
     # mensaje si se ingresa un tipo incorrecto o bien no se escribe bien el género. Devuelve el listado de géneros.
     if (type(anio)!= int) or (anio not in anios): 
-        return ("No se encuentra el año indicado. \nEste es un listado de los años para los cuales tenemos información: \n" + str(anios))
+        return ("No se encuentra el año indicado. Este es un listado de los años para los cuales tenemos información: " + str(anios))
     else:
         notreco1 = df_year_recommendations[df_year_recommendations['year']==anio]['not_recommend_title_1'].item()
         notreco2 = df_year_recommendations[df_year_recommendations['year']==anio]['not_recommend_title_2'].item()
@@ -66,7 +66,7 @@ def UsersNotRecommend( anio : int ):
 def sentiment_analysis( anio : int ):
     # mensaje si se ingresa un tipo incorrecto o bien no se escribe bien el género. Devuelve el listado de géneros.
     if (type(anio)!= int) or (anio not in anioslanzamiento): 
-        return ("No se encuentra el año indicado. \nEste es un listado de los años para los cuales tenemos información: \n" + str(anioslanzamiento))
+        return ("No se encuentra el año indicado. Este es un listado de los años para los cuales tenemos información: " + str(anioslanzamiento))
     else:
         Negativo = df_releaseyear_sentiment[df_releaseyear_sentiment['release_year']==anio]['Negative'].item()
         Neutral = df_releaseyear_sentiment[df_releaseyear_sentiment['release_year']==anio]['Neutral'].item()
@@ -86,4 +86,3 @@ def recomendacion_juego( product_id : int ):
         reco4=df_recommended_gamesxgames[df_recommended_gamesxgames['item_id']==product_id]['recommended_game_4'].item()
         reco5=df_recommended_gamesxgames[df_recommended_gamesxgames['item_id']==product_id]['recommended_game_5'].item()
         return ('Para el juego elegido '+ juego +' se recomiendan los siguientes juegos : '+ reco1 + ', ' + reco2 + ', ' + reco3  + ', ' + reco4  + ', ' + reco5)
-
